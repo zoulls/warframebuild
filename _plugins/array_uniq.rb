5b903ca3f38221240d958f3cb3679d7322940215
zoulls <zanamixx@gmail.com>
2016-05-07 00:46:55 +0200

    Update categories_page Add weapon index Add jekyll_assets plugin Update _config.yaml to exlcude Gemfile during the generation fo site

--- _plugins/array_uniq.rb
+++ _plugins/array_uniq.rb
@@ -1,9 +1,9 @@
 module Jekyll
   module ArrayUniqFilter
     def to_array_uniq(str)
-      str.split(',').uniq
+      str.split.uniq
     end
   end
 end
 
-Liquid::Template.register_filter('array_uniq',Jekyll::ArrayUniqFilter)
+Liquid::Template.register_filter(Jekyll::ArrayUniqFilter)
